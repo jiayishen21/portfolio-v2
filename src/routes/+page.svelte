e8be8627c64cc96488a035b85f23a235431bcefc
jiayishen21 <lucasshen21@gmail.com>
2024-07-12 21:10:58 -0400

    basic seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-	import Footer from './Footer.svelte';
-	import Hero from './Hero.svelte';
-	import Projects from './Projects.svelte';
-	import Technologies from './Technologies.svelte';
-	import Work from './Work.svelte';
+	import Footer from '../components/Footer.svelte';
+	import Hero from '../components/Hero.svelte';
+	import Projects from '../components/Projects.svelte';
+	import Technologies from '../components/Technologies.svelte';
+	import Work from '../components/Work.svelte';
 </script>
 
 <svelte:head>
 	<title>Lucas Shen</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta
+		name="description"
+		content="Lucas's portfolio website. I display my work and projects here."
+	/>
+	<meta
+		name="keywords"
+		content="Lucas Shen, Portfolio, Fullstack Developer, Software Engineer, Frontend Developer, Backend Developer, Slime Scholars, University of Waterloo"
+	/>
+	<!-- graph tags for social media -->
+	<meta property="og:title" content="Lucas Shen's Portfolio" />
+	<meta
+		property="og:description"
+		content="Lucas's portfolio website. I display my work and projects here."
+	/>
+	<meta property="og:image" content="/og lucas portfolio.png" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://lucasshen.dev/" />
 </svelte:head>
 
 <div class="center-content">
